4b66fd7c49442aeca3f9def86f4303f6157ecc4e
DavidIncarbone <david.incarbone@gmail.com>
2024-11-07 14:49:28 +0100

    add yellow lamp when click to button

--- script/script.js
+++ script/script.js
@@ -1,2 +1,21 @@
 "use strict";
 console.clear();
+
+const button = document.querySelector("button");
+console.dir(button);
+const img = document.querySelector("img");
+console.dir(img);
+
+
+button.addEventListener("click", function () {
+
+    img.src = "./img/yellow_lamp.png";
+    button.innerHTML = "Spegni";
+}
+
+)
+
+console.log(button.innerHTML);
+
+
+
